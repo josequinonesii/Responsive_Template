900842e561d8a10c15fc7181a0d32468166823fd
Kevin Thornbloom <kthornbloom@gmail.com>
2013-06-08 15:09:43 -0400

    Version 2.0!
    Completely re-worked with 50% less extra junk, CSS that is 90% easier to
    style, and 30% more code comments. Oh, and 100% more random percentages.

---
+++ Responsive Template 2.0/js/corefunctions.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+		// Main dropdown functions
+	$(" .main-nav li").hover(function(){
+        $(this).find('ul:first').slideDown('fast', 'easeOutExpo');
+        	var elm = $('ul:first', this);
+		    var off = elm .offset();
+		    var l = off.left;
+		    var w = elm.width();
+		    var docW = $(".main-nav").width();
+		    var isEntirelyVisible = (l+ w <= docW);
+
+		    if ( ! isEntirelyVisible ) {
+		        $(this).find('ul').addClass('edge');
+		    } else {
+		        $(this).removeClass('edge');
+		    }
+        },function(){
+        $(this).find('ul:first').slideUp('fast', 'easeOutExpo');
+    });
+	// Find parents & add class
+	$('.main-nav li:has(> ul)').find(">:first-child").addClass('parent');
+	// Save main nav html
+	var navHtml = $('.main-nav').html();
+	// Populate mobile nav html
+	$('.mobile-nav').html(navHtml);
+	$('.mobile-nav .parent').parent().append('<div class="indicator"><i class="icon-chevron-down"></i></div>');
+	// Mobile Indicators
+	$('.mobile-nav .indicator').live("click", function() {
+		event.preventDefault();
+		$(this).addClass('indicator-active').removeClass('indicator');
+		$(this).html('<i class="icon-chevron-up"></i>');
+	    $(this).parent().parent().find('ul:first').slideToggle();
+	});
+
+	$('.mobile-nav .indicator-active').live("click", function() {
+		event.preventDefault();
+		$(this).addClass('indicator').removeClass('indicator-active');
+		$(this).html('<i class="icon-chevron-down"></i>');
+	    $(this).parent().parent().find('ul:first').slideToggle();
+	});
+
+
+});
